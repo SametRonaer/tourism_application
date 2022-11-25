5ffe62f18629f620bb1b6f75fe9b7c1d151e022a
Samet Ronaer <s.ronaer@hotmail.com>
2022-11-25 11:19:47 +0300

    Initial commit

---
+++ TourismApplication/TourismApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TourismApplication
+//
+//  Created by Samet Ronaer on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
