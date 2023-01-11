7c0e215b8eebdb5ed1525ad6f1959aacf110f54b
Samet Ronaer <s.ronaer@hotmail.com>
2023-01-11 03:15:09 +0300

    Tiny update made

--- TourismApplication/TourismApplication/LoginVC.swift
+++ TourismApplication/TourismApplication/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginVC.swift
 //  TourismApplication
 //
 //  Created by Samet Ronaer on 25.11.2022.
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBack(_ sender : UIButton){
+        dismiss(animated: true)
     }
 
 }
-
